fb8771fc13f33199021bef9aaaecfd33ba92d978
ren <raenierquiambao@gmail.com>
2024-05-09 16:41:48 +0800

    Add Pawn class for pawn specific pieces
    add a valid_move? method that checks valid move for pawn piece

---
+++ lib/pawn.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require_relative 'chess_piece'
+
+class Pawn < ChessPiece
+  def valid_move?(current_position, target_position, target_value = nil)
+    return false if (target_position[0] - current_position[0]).abs > 1
+
+    if target_position[0] == current_position[0]
+      return true if current_position[1] + 1 == target_position[1]
+      return true if current_position[1] + 2 == target_position[1] && initial_move
+    elsif target_value && target_value.color != color
+      return true
+    end
+
+    false
+  end
+end
