de8bc799c1c6f5fab240c743cf836c1572b6869a
ren <raenierquiambao@gmail.com>
2024-05-10 12:52:57 +0800

    Add method and attribute for displaying unicode characters of pieces

--- lib/chess_piece.rb
+++ lib/chess_piece.rb
@@ -1,8 +1,14 @@
 class ChessPiece
-  attr_accessor :color, :initial_move
+  attr_accessor :color, :initial_move, :unicode_symbol
 
   def initialize(color)
     self.color = color
     self.initial_move = true
+  end
+
+  def symbol
+    return unless unicode_symbol
+
+    unicode_symbol[color.to_sym]
   end
 end
