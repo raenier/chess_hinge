f7e70c76c98565ee3cec9f58abea631621f40413
ren <raenierquiambao@gmail.com>
2024-05-10 14:32:22 +0800

    Set initial position of pieces with their corresponding colors

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,16 +1,46 @@
 # frozen_string_literal: true
 
 require_relative './chess_pieces/pawn'
+require_relative './chess_pieces/rook'
+require_relative './chess_pieces/knight'
+require_relative './chess_pieces/bishop'
+require_relative './chess_pieces/king'
+require_relative './chess_pieces/queen'
 
 class Chess
   attr_accessor :board
 
   def initialize
     self.board = set_board
+    preset_pieces
   end
 
   def set_board
     Array.new(8) { Array.new(8) }
+  end
+
+  def preset_pieces
+    board.each_with_index do |column, index|
+      column[-2] = Pawn.new('white')
+      column[1] = Pawn.new('black')
+
+      if [0, 7].include?(index)
+        column[-1] = Rook.new('white')
+        column[0] = Rook.new('black')
+      elsif [1, 6].include?(index)
+        column[-1] = Knight.new('white')
+        column[0] = Knight.new('black')
+      elsif [2, 5].include?(index)
+        column[-1] = Bishop.new('white')
+        column[0] = Bishop.new('black')
+      elsif index == 3
+        column[-1] = Queen.new('white')
+        column[0] = Queen.new('black')
+      elsif index == 4
+        column[-1] = King.new('white')
+        column[0] = King.new('black')
+      end
+    end
   end
 
   def display_board
