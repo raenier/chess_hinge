96bdf0723ea966c16d94077cde8a214fb0da0fb1
ren <raenierquiambao@gmail.com>
2024-05-16 14:43:20 +0800

    Check valid moves for Knight pieces

--- lib/chess_pieces/knight.rb
+++ lib/chess_pieces/knight.rb
@@ -3,8 +3,27 @@
 require_relative '../chess_piece'
 
 class Knight < ChessPiece
+  MOVES = [
+    [2, 1],
+    [2, -1],
+    [1, 2],
+    [1, -2],
+    [-1, -2],
+    [-1, 2],
+    [-2, 1],
+    [-2, -1]
+  ].freeze
+
   def initialize(color)
     super(color)
     self.unicode_symbol = { white: "\u2658", black: "\u265E" }
+  end
+
+  def valid_move?(current_position, target_position, target_value)
+    return false if target_value&.color == color
+    return true if MOVES.include? [current_position.first - target_position.first,
+                                   current_position.last - target_position.last]
+
+    false
   end
 end
