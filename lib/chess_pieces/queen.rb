c486ca51c4059a4f4b7943990ebb78f025f90df9
ren <raenierquiambao@gmail.com>
2024-05-17 17:49:21 +0800

    Check valid moves for Queen

--- lib/chess_pieces/queen.rb
+++ lib/chess_pieces/queen.rb
@@ -7,4 +7,16 @@ class Queen < ChessPiece
     super(color)
     self.unicode_symbol = { white: "\u2655", black: "\u265B" }
   end
+
+  def valid_move?(current_position, target_position, target_value)
+    return false if target_value&.color == color
+
+    if (current_position.first - target_position.first).abs == (current_position.last - target_position.last).abs
+      return true
+    elsif current_position.first == target_position.first || current_position.last == target_position.last
+      return true
+    end
+
+    false
+  end
 end
