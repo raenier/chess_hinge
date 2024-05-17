622382e90178979e6ebd640f5e02ebcf89845bc0
ren <raenierquiambao@gmail.com>
2024-05-17 17:33:33 +0800

    Check valid moves for Bishop

--- lib/chess_pieces/bishop.rb
+++ lib/chess_pieces/bishop.rb
@@ -7,4 +7,13 @@ class Bishop < ChessPiece
     super(color)
     self.unicode_symbol = { white: "\u2657", black: "\u265D" }
   end
+
+  def valid_move?(current_position, target_position, target_value)
+    return false if target_value&.color == color
+    if (current_position.first - target_position.first).abs == (current_position.last - target_position.last).abs
+      return true
+    end
+
+    false
+  end
 end
