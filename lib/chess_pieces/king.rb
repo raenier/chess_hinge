d5590a58b061ed9b6cce950554d948060659286c
ren <raenierquiambao@gmail.com>
2024-05-18 12:37:47 +0800

    Check valid moves for King

--- lib/chess_pieces/king.rb
+++ lib/chess_pieces/king.rb
@@ -7,4 +7,18 @@ class King < ChessPiece
     super(color)
     self.unicode_symbol = { white: "\u2654", black: "\u265A" }
   end
+
+  def valid_move?(current_position, target_position, target_value)
+    return false if target_value&.color == color
+
+    if current_position.first == target_position.first && (current_position.last - target_position.last).abs == 1
+      return true
+    elsif current_position.last == target_position.last && (current_position.first - target_position.first).abs == 1
+      return true
+    elsif (current_position.first - target_position.first).abs == 1 && (current_position.last - target_position.last).abs == 1
+      return true
+    end
+
+    false
+  end
 end
